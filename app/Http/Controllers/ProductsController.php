b3ccafd239fc9fa656f5b89b94e439a0537d4eb2
Gustavo Tavares <gtbarbosa@live.com>
2019-06-02 19:05:26 -0300

    first commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductsRequest as Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $minutes = \Carbon\Carbon::now()->addMinutes(10);
+        return \Cache::remember('api::products', $minutes, function(){
+            return Product::all();
+        });
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        \Cache::forget('api::products');
+        $user_id = \Auth::id();
+        $request->merge(['user_id' => $user_id]);
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return $product;
+    }
+}
